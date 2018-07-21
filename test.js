509ebe908be5eb99f41de20ab1e834b9ac2eca28
Ahmet Simsek <iamaroott@gmail.com>
2018-07-21 14:45:05 +0300

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient
 
 const uri = 'mongodb://localhost:27017'
 
-test('layer 0 test', async t => {
+test('#test', async t => {
 
   // connect mongo
   const client = await MongoClient.connect(uri, {useNewUrlParser: true})
